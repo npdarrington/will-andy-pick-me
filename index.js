be401b8ee6df569595df8032fcc501419c605028
Nathan Darrington <npdarrington@icloud.com>
2020-07-24 10:25:58 -0600

    Added clickHandler function to handler user on click to return question and answer

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var userInput = document.querySelector('.input-question');
 var answerBtn = document.querySelector('.btn-answer');
 var eightBallImg = document.querySelector('.eight-ball');
-var returnQuestion = document.querySelector('.question-return'):
+var returnQuestion = document.querySelector('.question-return');
 var returnAnswer = document.querySelector('.answer-return');
+var askQuestionSection = document.querySelector('.ask-question');
 var answers = [
     'It is certain',
     'It is decidedly so',
@@ -25,3 +26,19 @@ var answers = [
     'Outlook not so good',
     'Very doubtful'
 ];
+
+askQuestionSection.addEventListener('click', clickHandler);
+
+function clickHandler(event) {
+  if (event.target.classList.contains('btn-answer')) {
+    var question = userInput.value;
+    userInput.value = '';
+    eightBallImg.classList.add('hidden');
+    returnQuestion.innerText = `${question}?`;
+    returnAnswer.innerText = answers[getRandomIndex(answers)];
+  }
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
